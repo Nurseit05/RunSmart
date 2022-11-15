d302c8f8ce33c2c73e55b02b44dfecb72bbf86b2
Nurseit <nurseyit2005@gmail.com>
2022-11-15 17:17:52 +0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,89 @@
+//( var name = "Nurseit"; устаревший var
+// let number = 7; // let мы можем когда-то помняться информацию заданию внутри number
+// const pi = 3.14; const мы не можем поменять информацию заданию внутр pi
+
+// let leftBorderWidth = 200; переменая сперва пишем с маленькой букв если добавим слова то с большой
+
+// типы данные 
+// number - число для обозночение заплаты, количество px в общем что можно измерить количественно
+// string - можем поместить абсолютно люблю информацию описать словами. как страку - "", '', ``
+// true/false 
+// null - короче чего не существует в коде.
+// undefined - что-то существует но значение не имеет. пример let git;
+
+//alert(1234)
+// console.log(number); ответ выводит в консоле разработчика
+
+// confirm("Вам есть 18?");  справишиваем у пользовталя 
+// let answer = prompt("Вам есть 18?", ""); спрашиваем у пользователя и он может писать
+// console.log(answer);
+
+// console.log(4 + 4);
+
+
+// let isChecked = true,
+//     isClose= false;
+// && оператор И
+// console.log(isChecked && isClose);
+
+// || логические операторы ИЛИ
+// console.log(isChecked || isClose);
+
+// условия if или else
+// if (2 * 4 == 8 * 1){
+//   console.log('верно');
+// } else {
+//   console.log('ошибка')
+// }
+
+// let answer = confirm("Вам есть 18?");
+// if (answer){
+//   console.log('проходите');
+// } else {
+//   console.log('уходите');
+// }
+
+// const num = 50;
+
+// if (num < 49){
+//   console.log('неправильно');
+// } else if (num > 100){
+//   console.log('много');
+// } else {
+//   console.log('верно');
+// }
+
+
+
+// for() - цикл
+// for (let i = 1; i < 8;i++){
+//   console.log(i);
+// }
+
+
+
+// Функция
+// function logging(){
+//   console.log(222);
+// }
+// logging();)
+
+
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    speed: 1000, 
+    adaptiveHeight: true,
+    autoplay: false,
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
+  });
+});
